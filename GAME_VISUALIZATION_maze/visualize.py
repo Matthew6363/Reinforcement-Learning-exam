8512a923b6017e5a43952c27c36e5021d527cf12
MatteoLiotta <matteoliotta2002@gmail.com>
2026-05-20 18:06:30 +0200

    better

--- GAME_VISUALIZATION_maze/visualize.py
+++ GAME_VISUALIZATION_maze/visualize.py
@@ -287,7 +287,7 @@ def visualize_trained_agents(model_path='../CODE/EXPORT/q_models.npz',
 
 if __name__ == "__main__":
 
-    FILE_NAME = 'q_models_maze_ep500'    
+    FILE_NAME = 'q_models_maze_ep6500'    
     
     visualize_trained_agents(f"../EXPORT/{FILE_NAME}.npz", 
                              checkpoint=0,
